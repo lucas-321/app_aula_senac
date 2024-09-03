355bb69e9c5614ce3b6621db713fcfcd15ed0600
lucas-321 <38102320+lucas-321@users.noreply.github.com>
2024-09-03 18:46:36 -0300

    Add files via upload

--- www/js/routes.js
+++ www/js/routes.js
@@ -29,6 +29,25 @@ var app = new Framework7({
           spaceBetween: 30,
           autoplay: true,
           delay: 3000,
+          loop: true,
+          breakpoints: {
+            50: {
+              slidesPerView:1,
+              spaceBetween: 30,
+            },
+            640: {
+              slidesPerView:2,
+              spaceBetween: 30,
+            },
+            992: {
+              slidesPerView:3,
+              spaceBetween: 30,
+            },
+            1200: {
+              slidesPerView:4,
+              spaceBetween: 30,
+            },
+          }
         });
       },
       pageAfterIn: function (event, page) {
