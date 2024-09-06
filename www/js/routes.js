dae3cfbc87a92fef04d0d338f99535cf59f7d6e8
lucas-321 <38102320+lucas-321@users.noreply.github.com>
2024-09-06 18:33:19 -0300

    Add files via upload

--- www/js/routes.js
+++ www/js/routes.js
@@ -24,6 +24,7 @@ var app = new Framework7({
       on: {
       pageBeforeIn: function (event, page) {
         // fazer algo antes da página ser exibida
+        $("#menuPrincipal").show("fast");
         var swiper = new Swiper(".mySwiper", {
           slidesPerView: 1,
           spaceBetween: 30,
@@ -122,7 +123,6 @@ var app = new Framework7({
 		},
 	  }
     },
-
     {
       path: '/link4/',
       url: 'link4.html',
@@ -141,6 +141,26 @@ var app = new Framework7({
 		// fazer algo antes da página ser removida do DOM
 		},
 	  }
+    },
+    {
+      path: '/detalhes/',
+      url: 'detalhes.html',
+      animate: false,
+      on: {
+      pageBeforeIn: function (event, page) {
+      // fazer algo antes da página ser exibida
+      $("#menuPrincipal").hide("fast");
+      },
+      pageAfterIn: function (event, page) {
+      // fazer algo depois da página ser exibida
+      },
+      pageInit: function (event, page) {
+      // fazer algo quando a página for inicializada
+      },
+      pageBeforeRemove: function (event, page) {
+      // fazer algo antes da página ser removida do DOM
+      },
+      }
     },
   ],
   // ... other parameters
